ac741840f747c0fe5b4ba61658f2bb45bccc8442
Edanur Meriç <119623069+edanurmeric@users.noreply.github.com>
2023-06-07 18:02:55 +0300

    Add files via upload

---
+++ Script/characterController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody2D r2d;
+    private Animator _animator;
+    private Vector3 charPos;
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private GameObject _camera;
+    
+
+    void Start()
+    {
+    
+        r2d = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        charPos = transform.position;
+        _spriteRenderer = GetComponent<SpriteRenderer>(); //caching spriteRenderer
+
+    }
+    private void FixedUpdate() //50 fps
+    {
+       
+        r2d.velocity = new Vector2(x: speed, y: 0f);
+        
+    }
+    void Update()
+    {
+     
+        
+        charPos = new Vector3(x: charPos.x + (Input.GetAxis("Horizontal") * speed * Time.deltaTime), charPos.y);
+        transform.position = charPos; //Hesapladýðým pozisyon karakterime iþlensin
+       
+        if (Input.GetAxis("Horizontal") == 0.0f)
+        {
+            _animator.SetFloat(name: "speed", value: 0.0f);
+        }
+        else
+        {
+            _animator.SetFloat("speed", speed);
+        }
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (Input.GetAxis("Horizontal") < -0.01f)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        
+        
+    }
+    private void LateUpdate()
+    {
+        _camera.transform.position = new Vector3(charPos.x, charPos.y, charPos.z - 1.0f);
+        
+    }
+}
